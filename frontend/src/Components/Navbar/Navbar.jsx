7723bdb346da6bbfb1e22d24cf4bb895c02370bd
Sujit Yalmar <sujityalmar02@gmail.com>
2024-11-14 00:19:22 +0530

    Add file

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,59 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-import menu_icon from '../../assets/menu-icon.png'
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 import { Link } from 'react-scroll';
 import { useAuth0 } from "@auth0/auth0-react";
 
-
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
-    const { loginWithRedirect,  isAuthenticated, logout, user } = useAuth0();
-    useEffect(()=>{
-        window.addEventListener('scroll', ()=>{
-            window.scrollY > 50 ? setSticky(true) : setSticky(false);
-        })
-    },[]);
+    const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
     const [mobileMenu, setMobileMenu] = useState(false);
-    const toggleMenu = ()=>{
-      mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
-    }
-  return (
-    <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-      <img src={logo} alt="" className='logo' />
-      <ul className={mobileMenu?'':'hide-mobile-menu'}>
-        <li>
-          <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link>
-          </li>
-        <li>
+    const [showDropdown, setShowDropdown] = useState(false);
 
-          <Link to='program' smooth={true} offset={-260} duration={500}>Feature</Link>
-          </li>
-        <li>
-          <Link to='about' smooth={true} offset={-150} duration={500}>About us</Link>
-          </li>
-        <li>
-          <Link to='testimonials' smooth={true} offset={-260} duration={500}>Feedback</Link>
-          </li>
-        <li>
-          <Link to='contact' smooth={true} offset={-260} duration={500}>Contact Us</Link>
-          </li>
-        {/* <li><Link to='' smooth={true} offset={-260} duration={500} className='btn'>Login</Link></li> */}
-        <li>{isAuthenticated && <p>{user.name} </p>}</li>
-        {isAuthenticated ?(
-          <li>
-            <button className='btn' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-              Log Out
-            </button>
-          </li>
-        ):(
-          <li>
-          <button className='btn' onClick={() => loginWithRedirect()}>Log In</button>
-          </li>
-        )}  
-      </ul>
-      <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
-    </nav>
-  )
-}
-export default Navbar
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 50 ? setSticky(true) : setSticky(false);
+        });
+    }, []);
+
+    const toggleMenu = () => {
+        setMobileMenu(!mobileMenu);
+    };
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
+
+    return (
+        <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+            <img src={logo} alt="Logo" className='logo' />
+            <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='program' smooth={true} offset={-260} duration={500}>Feature</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Feedback</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500}>Contact Us</Link></li>
+                
+                {isAuthenticated && (
+                    <li className="user-profile">
+                        <div onClick={toggleDropdown} className="profile-container">
+                            <img src={user.picture} alt="Profile" className="profile-pic" />
+                        </div>
+                        {showDropdown && (
+                            <div className="dropdown-menu">
+                                <div className="profile-header">
+                                    <img src={user.picture} alt={user.name} className="profile-large-pic" />
+                                    <div className="profile-info">
+                                        <h3>{user.name}</h3>
+                                        <p>{user.email}</p>
+                                    </div>
+                                </div>
+                                <hr />
+                                <button className='btn profile-btn' onClick={() => window.location.href = '/profile-settings'}>
+                                    Profile Settings
+                                </button>
+                                <button
+                                    className='btn profile-btn logout-btn'
+                                    onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+                                >
+                                    Log Out
+                                </button>
+                            </div>
+                        )}
+                    </li>
+                )}
+                {!isAuthenticated && (
+                    <li>
+                        <button className='btn' onClick={() => loginWithRedirect()}>Log In</button>
+                    </li>
+                )}
+            </ul>
+            <img src={menu_icon} alt="Menu Icon" className='menu-icon' onClick={toggleMenu} />
+        </nav>
+    );
+};
+
+export default Navbar;
